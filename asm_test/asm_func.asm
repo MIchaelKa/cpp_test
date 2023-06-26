9c34bf52f2c9f6adf78f2bf2933ec4066969fdc0
michaelka <michael.kalinin.nn@gmail.com>
2023-06-27 00:50:52 +0300

    exchange

--- asm_test/asm_func.asm
+++ asm_test/asm_func.asm
@@ -1,6 +1,7 @@
 
 .globl _sum_two
 .globl _compare_exchange
+.globl _exchange
 
 _sum_two:
     movq %rdi, %rax
@@ -8,10 +9,13 @@ _sum_two:
     retq
 
 _compare_exchange:
-
     # cmpxchg (%rdi), %rsi
     # cmpxchg $5, %rsi
-
     movq %rdi, %rax
     cmpxchg %eax, %esi
+    retq
+
+_exchange:
+    movq (%rdi), %rax
+    xchg %rsi, (%rdi)
     retq
--- asm_test/main.cpp
+++ asm_test/main.cpp
@@ -5,6 +5,8 @@ extern "C" int64_t sum_two(int64_t a, int64_t b);
 
 extern "C" int64_t compare_exchange(int64_t a, int64_t b);
 
+extern "C" int64_t exchange(int64_t* old_value, int64_t new_value);
+
 int main() {
     int64_t result = sum_two(5, 6);
     std::cout << "sum_two: " << result << std::endl;
@@ -12,6 +14,11 @@ int main() {
     int64_t first = 11;
     int64_t a = compare_exchange(first, 11);
     std::cout << "compare_exchange: " << a << std::endl;
+
+    int64_t old_v = 7;
+    int64_t new_v = 15;
+    int64_t res = exchange(&old_v, new_v);
+    std::cout << "exchange: " << res << ", " << old_v << "/" << new_v << std::endl;
 
     return 0;
 }
